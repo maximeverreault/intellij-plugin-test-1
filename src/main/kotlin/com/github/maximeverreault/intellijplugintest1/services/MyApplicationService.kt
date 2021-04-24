1ba61b89e147e2389058239e5f21d4671b3369aa
GitHub Action <action@github.com>
2021-04-24 17:36:59 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/maximeverreault/intellijplugintest1/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.maximeverreault.intellijplugintest1.services
+
+import com.github.maximeverreault.intellijplugintest1.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/maximeverreault/intellijplugintest1/services/MyProjectService.kt
+++ src/main/kotlin/com/github/maximeverreault/intellijplugintest1/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.maximeverreault.intellijplugintest1.services
 
+import com.github.maximeverreault.intellijplugintest1.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
